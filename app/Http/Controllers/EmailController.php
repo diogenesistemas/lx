b8ff048cca36ce7eed2b6d4829c7d1c21d3c2b79
diogenesistemas <ads.diogenes@gmail.com>
2016-09-18 21:13:16 -0300

    Enviando Email

---
+++ app/Http/Controllers/EmailController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Mail;
+
+class EmailController extends Controller
+{
+    private $mail;
+
+    public function __construct(Mail $mail){
+    	$this->mail = $mail;
+
+    }
+
+    public function sendMail(){
+
+    	// return "oi";
+ 
+    	// $this->mail->send(null, [], function ($m) {
+    	// 	$m->to("ads.diogenes@gmail.com", "Esse é um teste de envio de email");
+    	// });
+        Mail::send('mail.teste-email', [], function ($message) {    	
+
+        // Mail::send('mail.teste-email', [], function ($message) {   
+
+            $message->to("ads.diogenes@gmail.com", "Teste")->subject('Teste Email');
+            $message->attach('C:\Users\Diogenes\Desktop\testelx.docx', ['as' => 'testelx', 'mime' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document']);
+        });
+        return "enviado";
+    }
+}
+
+
+ // Mail::send('emails.reminder', ['user' => $user], function ($m) use ($user) {
+ //            $m->from('hello@app.com', 'Your Application');
+
+ //            $m->to($user->email, $user->name)->subject('Your Reminder!');
+ //        });
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/email', ['as' => 'mail', 'uses' =>'EmailController@sendMail']);
