0a30b788e0b33fd21507436f0a6600653fceb9e2
Georgines <geoginae.p2@gmail.com>
2016-11-14 16:54:00 -0300

    -Modificacao correcao de erro uploadService

--- app/Services/UploadServices.php
+++ app/Services/UploadServices.php
@@ -44,9 +44,9 @@ class UploadServices
                     $this->originalName, $this->file->getClientMimeType(),
                     $this->file->getClientOriginalExtension(), $this->path
                 );
-                $file_db = $this->getFileInfoToDatabase();
+                $file_db =  $this->getFileInfoToDatabase();
 
-                return $this->response('Upload Completo!', true, $file_db->id);
+                return $this->response('Upload Completo!', true, $file_db['id']);
 
             } else {
                 return $this->response('Falha no upload!');
@@ -94,6 +94,8 @@ class UploadServices
                 'extension' => $extension,
                 'path' => $path,
             ]);
+
+
         }
     }
 
