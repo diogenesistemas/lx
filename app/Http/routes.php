22417d731cc2bc395fca2db7d0beaa8e5e652669
Georgines <geoginae.p2@gmail.com>
2016-11-14 02:05:42 -0300

    -Adicionado rota para verificar se o arquivo existe

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Storage;
+
 Route::get('/', ['as' => 'index', 'uses' => 'MainController@goIndex']);
 Route::get('/resetar-sessao', ['as' => 'regenerate-session', 'uses' => 'UploadController@regenerateSession']);
 
@@ -7,4 +9,10 @@ Route::post('/upload-files', ['as' => "upload-files", 'uses' => 'UploadControlle
 Route::delete('/upload-files/delete/{id}', ['as' => "delete-files", 'uses' => 'UploadController@delete']);
 
 Route::get('/email', ['as' => 'mail', 'uses' =>'EmailController@sendMail']);
+Route::get('/file', function(){
+    if(Storage::disk('upload')->exists('2016-11-14_01-11-42_4.pptx')){
+        return 'existe';
+    }
+    return 'não existe';
+});
 
